c964617969d4733c58d2adc411724b930fd87e04
eunicelok <eunicelp511@gmail.com>
2020-02-19 13:28:44 +0800

    Added devise for customer, yet to add logout for customer and have buttons that separate logins for both customer and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :customers, path: 'customers', controllers: { sessions: "customers/sessions", registrations: "customers/registrations" }
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions", registrations: "users/registrations" }
   resources :listings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'listings#landingpage'
   get '/all' => 'listings#index'
+  # tentatively when customers sign in, it'll bring them to listings pages first. will change their routes to smth else
+  get 'customers/:id' => 'listings#index'
 
 
 end
