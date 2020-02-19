a56615ff6edd161a1ca5972dc4f9e8ae3b8b8796
joycemap <57893220+joycemap@users.noreply.github.com>
2020-02-19 17:19:03 +0800

    search bar fully works

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
     belongs_to :user
 # http://www.ryanmcmahon.org/articles/11
     include PgSearch::Model
-  pg_search_scope :search_by_listings, :against => [:name, :address, :postal_code],
+  pg_search_scope :search_by_listings, :against => [:name, :address, :postal_code, :email, :description],
     using: {
       :tsearch => {:prefix => true}
     }
